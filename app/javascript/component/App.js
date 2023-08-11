9eee312cf6919109db92d62fbe71c242d81a320c
evillaz <evillaz123@gmail.com>
2023-08-11 10:15:20 -0500

    Create react rails random greeting

---
+++ app/javascript/component/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function RandomGreeting() {
+  const [randomGreeting, setRandomGreeting] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/api/random_greeting')
+      .then(response => response.json())
+      .then(data => setRandomGreeting(data.greeting))
+      .catch(error => console.error('Error fetching greeting:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Greeting</h1>
+      <p>{randomGreeting}</p>
+    </div>
+  );
+}
+
+export default RandomGreeting;
